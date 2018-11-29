0cb8413dcd4ccad4cb3930edfa555111847e902b
Jordan Walsh <jordanwalsh9519@gmail.com>
2018-11-29 02:16:17 +0000

    Initial commit

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+#include "Screens.h"
+#include "Screen.h"
+#include "SFML\Graphics.hpp"
+
+int main() {
+
+	sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!");
+
+	std::vector<Screen> screens;
+	int currentScreen = 0;
+
+	return 0;
+}
